dde6c7a5df914c722e538d87e165ba8dc05019db
RainChain-Zero <rainchain@foxmail.com>
2022-03-15 19:39:56 +0800

    build docs

---
+++ dist/assets/Layout.4407d4c0.js
@@ -0,0 +1,1 @@
+import{g as r,n as m,j as a,r as e,p as n,q as s}from"./app.fe1c599e.js";import{S as p}from"./SkipLink.5088cd7d.js";var i=r({name:"Layout",setup(){const o=s(),t=m();return()=>[a(p),a(e("CommonWrapper"),()=>t.value.home?a(e("HomePage")):a(n,()=>a(e("NormalPage"),{key:o.value.path})))]}});export{i as default};
