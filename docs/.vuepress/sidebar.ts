fbc98f8e873067c3492c4e19f987a7ae955f1c33
RainChain-Zero <rainchain@foxmail.com>
2022-06-28 19:33:42 +0800

    uodate V4.7.8

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -40,7 +40,7 @@ export default defineSidebarConfig([
     icon: "note",
     prefix: "manual/nonebot2/",
     collapsable: true,
-    children: ["nbnhhsh","drawcard","guessoperator","bilibili","setu","tarot","shindanmaker","petpet","ygo"],
+    children: ["nbnhhsh","drawcard","guessoperator","bilibili","setu","tarot","shindanmaker","petpet","ygo","picsearcher"],
   },
   {
     text: "附录",
