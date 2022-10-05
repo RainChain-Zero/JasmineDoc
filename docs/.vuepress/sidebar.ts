b23e5a83f801222a062b3d74d7ec5202e170cc34
RainChain-Zero <rainchain@foxmail.com>
2022-10-05 15:54:35 +0800

    feat: 超分辨率重建

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -48,7 +48,7 @@ export default defineSidebarConfig([
     prefix: "manual/nonebot2/",
     collapsable: true,
     children: ["nbnhhsh", "drawcard", "guessoperator", "bilibili", "setu", "tarot", "shindanmaker",
-      "petpet", "ygo", "picsearcher", "caiyun", "minesweeper", "oddtext", "drawimage"],
+      "petpet", "ygo", "picsearcher", "caiyun", "minesweeper", "oddtext", "drawimage", "rebuildimg"],
   },
   {
     text: "Koishi",
